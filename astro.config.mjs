7ca8d8256e4dce0de3fd160bd5688018d9bbbf16
Maxi <claudioacevedo25@gmail.com>
2026-03-07 20:21:33 +0100

    chore: add basic project config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+      domains: ["images.unsplash.com"],
+	},
+
+  i18n: {
+      defaultLocale: "es",
+      locales: ["es", "en"],
+      routing: {
+          prefixDefaultLocale: false,
+      },
+	},
+
+  integrations: [icon()],
+})
